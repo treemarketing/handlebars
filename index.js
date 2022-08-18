c3c7237f520734a6772f4de94d37de32e40404d4
treemarketing <treemarketingonline@gmail.com>
2022-08-17 23:28:28 -0300

    cambios

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const server = app.listen(PORT, () => {
 server.on('error', (error) => console.log(`Error en servidor ${error}`));
 app.use('/public', express.static(__dirname + '/public'));
 
+//configuracion del motor
 app.set('view engine', 'hbs');
 app.set('views', './views');
 app.engine(
@@ -21,6 +22,7 @@ app.engine(
     partialsDir: __dirname + '/views/partials',
   })
 );
+// fin configuracion del motor
 
 let productsHC = [
   { id: 1, title: 'nike ball', price: 101, thumbnail: 'http://localhost:8080/public/nike-ball.jpg' },
@@ -28,7 +30,30 @@ let productsHC = [
   { id: 3, title: 'adidas shoes', price: 102, thumbnail: 'http://localhost:8080/public/adidas-shoes.jpg' },
 ];
 
-app.get('/', (req, res) => {
+app.get('/products/', (req, res) => {
   //sirve productslist.hbs en index.hbs (index.hbs es la plantilla por defecto donde arranca todo)
   res.render('productslist', { products: productsHC, productsExist: true });
+});
+
+app.get('/products/:id', (req, res) => {
+  const {id} = req.params;
+
+
+  try{
+    let found = productsHC.find(e => e.id == id)
+    if (found){
+      res.render('oneproduct', { product: found, tittle: found.title})
+
+    }else{
+      res.render('errorplantilla', { errorMessage: "Producto No Encontrado"})
+    }
+  }catch{
+    console.log(err)
+  }
+
+})
+
+app.get('/form', (req, res) => {
+  
+  res.render('form', {  });
 });
